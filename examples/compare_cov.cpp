dec4da93957f17b1987028e2ecbb770630d63f25
lauraprrrr <laura.montaner@mail.udp.cl>
2026-07-20 18:44:41 -0400

    feat: add script to compare .cov files

---
+++ examples/compare_cov.cpp
@@ -0,0 +1,31 @@
+// Archivo: compare_cov.cpp
+#include "ibex_CovOptimData.h"
+#include <iostream>
+
+void compare_cov_files(const char* file1, const char* file2) {
+    try {
+        ibex::CovOptimData data1(file1);
+        ibex::CovOptimData data2(file2);
+
+        std::cout << "=== COMPARACIÓN DE ARCHIVOS COV ===" << std::endl;
+        std::cout << "Métrica\t\t\tArchivo 1 (" << file1 << ")\tArchivo 2 (" << file2 << ")" << std::endl;
+        std::cout << "----------------------------------------------------------------------" << std::endl;
+        std::cout << "Tiempo (s):\t\t" << data1.time() << "\t\t\t" << data2.time() << std::endl;
+        std::cout << "Nº Celdas/Nodos:\t" << data1.nb_cells() << "\t\t\t" << data2.nb_cells() << std::endl;
+        std::cout << "Cota Inferior (uplo):\t" << data1.uplo() << "\t\t\t" << data2.uplo() << std::endl;
+        std::cout << "Cota Superior (loup):\t" << data1.loup() << "\t\t\t" << data2.loup() << std::endl;
+        std::cout << "Cajas guardadas:\t" << data1.size() << "\t\t\t" << data2.size() << std::endl;
+    } catch (std::exception& e) {
+        std::cerr << "Error al abrir o procesar los archivos: " << e.what() << std::endl;
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cout << "Uso: " << argv[0] << " <archivo1.cov> <archivo2.cov>" << std::endl;
+        return 1;
+    }
+
+    compare_cov_files(argv[1], argv[2]);
+    return 0;
+}
